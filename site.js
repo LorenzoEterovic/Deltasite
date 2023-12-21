6ee89096882084a88920e84fbc9867d5a682f5ee
LorenzoEterovic <eterovic.752@gmail.com>
2023-12-21 18:03:28 -0300

    home 0.06

--- site.js
+++ site.js
@@ -1,3 +1,25 @@
 function update(){
 document.getElementById("demo").style.borderRadius = "20%";
 };
+
+function year1(){
+   document.getElementById("gy1").style.backgroundColor = "gray";
+   document.getElementById("gy2").style.backgroundColor = "white";
+};
+
+function year2(){
+    document.getElementById("gy2").style.backgroundColor = "gray";
+    document.getElementById("gy1").style.backgroundColor = "white";
+ };
+
+ function year(){
+    if(onclick=("gy1")){
+        document.getElementById("1%").innerHTML = "30%";
+        document.getElementById("2%").innerHTML = "39%";
+        document.getElementById("3%").innerHTML = "27%";
+    }
+ }
+
+
+
+
